5e4f27ec5c8fccb55282badabda580fbbf54adb6
ShIreen66 <066shiree@gmail.com>
2024-10-13 22:54:59 +0530

    solution 4

--- Questions/script.js
+++ Questions/script.js
@@ -22,8 +22,17 @@
 // 3. Accept two integers from user and print the sum
 	// Ex - The sum of 45 & 12 = 57
 
-    let a = +prompt("Enter first value")
-    let b = +prompt("Enter second value")
-    let sum = a + b 
-    console.log(`The sum of ${a} & ${b} = ${sum}`)
+    // let a = +prompt("Enter first value")
+    // let b = +prompt("Enter second value")
+    // let sum = a + b 
+    // console.log(`The sum of ${a} & ${b} = ${sum}`)
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Question 4 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// 4. Accept the User's name, age and print in following manner
+	// Ex - Hello Shery, you are 12 years old.
+
+    let name = prompt("Enter your name")
+    let age = prompt("Enter your age")
+    console.log(`Hello ${name} you are & ${age} years old`)
 
