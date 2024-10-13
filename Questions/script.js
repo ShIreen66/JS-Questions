185e736a7230ef4bc3ffa78984605ae2c7a09c39
ShIreen66 <066shiree@gmail.com>
2024-10-13 22:49:23 +0530

    solution 3

--- Questions/script.js
+++ Questions/script.js
@@ -12,7 +12,18 @@
 // 2. Print the messege along with values and sum
 	// Ex - The sum of 45 & 12  = 57
 
-let a = 45
-let b = 12 
-let sum = a + b
-console.log(sum);
+// let a = 45
+// let b = 12 
+// let sum = a + b
+// console.log(sum);
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Question 3 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// 3. Accept two integers from user and print the sum
+	// Ex - The sum of 45 & 12 = 57
+
+    let a = +prompt("Enter first value")
+    let b = +prompt("Enter second value")
+    let sum = a + b 
+    console.log(`The sum of ${a} & ${b} = ${sum}`)
+
