027ebb849674951761369888ad1f2e20668f6e4e
ShIreen66 <066shiree@gmail.com>
2024-10-13 22:40:34 +0530

    solution 2

--- Questions/script.js
+++ Questions/script.js
@@ -1,5 +1,18 @@
-// - Sum of two integers
-let a = 10
-let b = 10 
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Question 1 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// 1. Sum of two integers
+
+// let a = 10
+// let b = 10 
+// let sum = a + b
+// console.log(sum);
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Question 2 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// 2. Print the messege along with values and sum
+	// Ex - The sum of 45 & 12  = 57
+
+let a = 45
+let b = 12 
 let sum = a + b
 console.log(sum);
