9e8761ac96ea2755df7a55c9bbb0c1356af5a658
ShIreen66 <066shiree@gmail.com>
2024-10-14 23:35:15 +0530

    solution 8

--- Questions/script.js
+++ Questions/script.js
@@ -69,7 +69,22 @@
 
 // 7. Accept the length and width of a rectangle. Calculate & print the area and perimiter.
 
-let length = +prompt("Enter the length of rectangle")
-let width = +prompt("Enter the brath of rectangle")
-console.log(`The area of rectangle is ${length*width}`)
-console.log(`The perimiter of rectangle is ${2*(length+width)}`)
+// let length = +prompt("Enter the length of rectangle")
+// let width = +prompt("Enter the brath of rectangle")
+// console.log(`The area of rectangle is ${length*width}`)
+// console.log(`The perimiter of rectangle is ${2*(length+width)}`)
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Question 8 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// Accept the parameters and calculate the Compound Interest & print it on STDOUT (Use Math class methods)
+
+let p = +prompt("Enter the principle amount")
+let r = +prompt("Enter the rate of interest")/100
+let t = +prompt("Enter the time in years")
+let n = +prompt("Enter the number of times interest applied per time period")
+
+
+let ci = p*Math.pow(1+r/n,t*n)
+let intrest = ci-p
+console.log("Simple interest :",intrest.toFixed(2))
+console.log("Total amount :",ci.toFixed(2))
